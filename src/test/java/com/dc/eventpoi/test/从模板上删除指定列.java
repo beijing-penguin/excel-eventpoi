bde4d550702b91daaa8748b98a97619c28ab368b
beijing-penguin <429544557@qq.com>
2021-08-28 15:07:41 +0800

    更新

--- src/test/java/com/dc/eventpoi/test/从模板上删除指定列.java
+++ src/test/java/com/dc/eventpoi/test/从模板上删除指定列.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 import com.dc.eventpoi.ExcelHelper;
 
-public class 删除指定列测试 {
+public class 从模板上删除指定列 {
     public static void main(String[] args) throws Exception {
         byte[] fileData = ExcelHelper.deleteTemplateColumn(Test1.class.getResourceAsStream("demo1Templete.xlsx"),"${age}");
         Files.write(Paths.get("./my_test_temp/从模板上删除指定列.xlsx"), fileData);
--- src/test/java/com/dc/eventpoi/test/使用列索引删除列测试.java
+++ src/test/java/com/dc/eventpoi/test/使用列索引删除列测试.java
@@ -9,7 +9,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import com.dc.eventpoi.ExcelHelper;
 
-public class 删除列测试 {
+public class 使用列索引删除列测试 {
     public static void main(String[] args) throws Exception {
         InputStream excelInput = Test1.class.getResourceAsStream("demo1.xlsx");
         Workbook workbook = WorkbookFactory.create(excelInput);
