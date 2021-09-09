40910377064c24a33879ab254e7721f7bd172f56
duanchao <429544557@qq.com>
2021-09-09 10:39:10 +0800

    支持图片/文本混合列的读取

--- src/test/java/com/dc/eventpoi/test/读取带图片的Excel文件.java
+++ src/test/java/com/dc/eventpoi/test/读取带图片的Excel文件.java
@@ -12,6 +12,7 @@ public class 读取带图片的Excel文件 {
         List<Person> objList = ExcelHelper.parseExcelToObject(Test1.class.getResourceAsStream("demo1.xlsx"), Test1.class.getResourceAsStream("demo1Templete.xlsx"), Person.class,true);
         System.out.println(JSON.toJSONString(objList,true));
         
+        System.err.println(new String(objList.get(2).getHeadImage()));
         //写到本地，并查看图片
         Files.write(Paths.get("./my_test_temp/my_image.jpg"), objList.get(0).getHeadImage());
     }
