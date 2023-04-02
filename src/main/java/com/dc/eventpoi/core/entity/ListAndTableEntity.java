4f54db9fb23002888d7b6079290093e15f68c5de
beijing-penguin <429544557@qq.com>
2023-04-02 13:06:17 +0800

    1

--- src/main/java/com/dc/eventpoi/core/entity/ListAndTableEntity.java
+++ src/main/java/com/dc/eventpoi/core/entity/ListAndTableEntity.java
@@ -1,18 +1,21 @@
 package com.dc.eventpoi.core.entity;
 
-import java.util.Arrays;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListAndTableEntity {
 	/**
 	 * 表单集合
 	 */
-	private List<?> tableList;
+	private List<Map<?, ?>> tableList;
 	
 	/**
 	 * 数据集合
 	 */
-	private List<?> dataList;
+	private List<List<Map<?, ?>>> dataList = new ArrayList<>();
 
 	
 	public static ListAndTableEntity build() {
@@ -23,23 +26,39 @@ public class ListAndTableEntity {
 		return tableList;
 	}
 
-	public ListAndTableEntity setTableList(List<?> tableList) {
-		this.tableList = tableList;
+	public ListAndTableEntity setTableList(List<?> tableList) throws Throwable {
+		List<Map<?, ?>> tableMapList = new ArrayList<Map<?, ?>>();
+		for(Object obj : tableList) {
+			if(obj.getClass().getClassLoader() == null) {
+				LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+				Field[] v_obj_field_arr = obj.getClass().getDeclaredFields();
+				for (Field field : v_obj_field_arr) {
+			        field.setAccessible(true);
+			        String keyName = field.getName();
+			        map.put(keyName, field.get(obj));
+			    }
+				tableMapList.add(map);
+			}else {
+				tableMapList.add((Map<?, ?>)obj);
+			}
+		}
+		this.tableList = tableMapList;
 		return this;
 	}
 
-	public ListAndTableEntity setTableList(Object tableObject) {
-        this.tableList = Arrays.asList(tableObject);
-        return this;
-    }
 	
-	
-	public List<?> getDataList() {
+	public List<List<Map<?, ?>>> getDataList() {
 		return dataList;
 	}
 
 	public ListAndTableEntity setDataList(List<?> dataList) {
-		this.dataList = dataList;
+		for(Object obj : dataList) {
+			List<?> objList = (List<?>)obj;
+			for(Object oo : objList) {
+				
+			}
+			List<Map<?, ?>> newMapList = (List<?>)obj;
+		}
 		return this;
 	}
 	
